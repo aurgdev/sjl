c1f354a5c222e74d9f8344a9ca4d16268c9be086
Mohamed <mohamed77massawi@gmail.com>
2022-12-09 21:24:33 +0100

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {
+      colors: {
+        primary: "#8d2337",
+        secendary: "#004733",
+      },
+    },
+  },
+  plugins: [],
+};
